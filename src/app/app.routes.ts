e89187465afb7d585b89d9f1588677400493a357
Your Name <mohamaduhadijatu@gmail.com>
2025-07-02 22:53:00 -0400

    fix: add route for country details to route configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ErrorpageComponent } from './components/errorpage/errorpage.component';
+import { CountrydetailsComponent } from './components/countrydetails/countrydetails.component';
 
 export const routes: Routes = [
  {
     path:'',
     component: HomepageComponent
+ },
+ {
+    path:':id',
+    component: CountrydetailsComponent
  },
  {
     path: '**',
