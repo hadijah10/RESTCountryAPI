60cd816eddf6398380b6067e3917071f8d49a497
Your Name <mohamaduhadijatu@gmail.com>
2025-07-03 00:33:46 -0400

    feat: add actions and reducers for the country details

--- src/app/store/actions/selectCountry.action.ts
+++ src/app/store/actions/selectCountry.action.ts
@@ -1,3 +1,7 @@
 import { createAction, props } from "@ngrx/store";
+import { ISelectCountryByNameInterface } from "../../models/interfaces/restdata.interface";
 
-export const selectedCountry = createAction('[SelectCountry] Get country details',props<{countryname:string}>())
+export const selectedCountry = createAction('[SelectCountry] Get country details')
+export const getSelectedCountrySuccessByName = createAction('[Get Selected Country By Name] Sucess',props<{country:ISelectCountryByNameInterface[]}>())
+export const getSelectedCountrySuccessByCode = createAction('[Get Selected Country By Name] Sucess',props<{code:string}>())
+export const getSelectedCountryFailed = createAction('[Get Selected Country Failure] Denied',props<{error:string}>())
--- src/app/store/reducers/selectCountry.reducer.ts
+++ src/app/store/reducers/selectCountry.reducer.ts
@@ -1,12 +1,16 @@
 import { ISelectedCountry } from "../../models/interfaces/restdata.interface"
-import { selectedCountry } from "../actions/selectCountry.action"
+import { getSelectedCountryFailed,getSelectedCountrySuccessByName,selectedCountry } from "../actions/selectCountry.action"
 import { on,createReducer } from "@ngrx/store"
 
 export const initialState: ISelectedCountry={
-    selectedCountry:''
+    isLoading:false,
+    data:[],
+    error: null
 }
 
 export const SelectedCountryReducer = createReducer(
     initialState,
-    on(selectedCountry,(state,action) => ({...state,selectedCountry:action.countryname}))
+    on(selectedCountry,(state) => ({...state,isLoading:false})),
+    on(getSelectedCountrySuccessByName,(state,action) => ({...state, isLoading: false, data: action.country})),
+    on(getSelectedCountryFailed,(state,action) => ({...state,isLoading: false, error:action.error}))
 )
