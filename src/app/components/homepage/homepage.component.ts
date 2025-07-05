08eb69261ec86019ed16c217fd92108781341664
Your Name <mohamaduhadijatu@gmail.com>
2025-07-05 03:27:16 -0400

    feat: add search functionality

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -4,33 +4,44 @@ import { Store } from '@ngrx/store';
 import { loadCountries } from '../../store/actions/loadCountries.action';
 import { loadCountriesSelector } from '../../store/selectors/loadCountries.selector';
 import { ICountriesData } from '../../models/interfaces/restdata.interface';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, Observable } from 'rxjs';
 import { IAppStateInterface } from '../../models/interfaces/appstate.interface';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink,FormsModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
   route = inject(Router)
   data$:Observable<ICountriesData>;
+  searchTerm =new BehaviorSubject<string>('')
+  filterRegion: string=''
+
   constructor(private store:Store<IAppStateInterface>){
-   
     this.data$ = this.store.select(loadCountriesSelector)
+    this.searchTerm.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+    ).subscribe({
+      next:(data => {
+      
+      })
+    })
 
   }
   ngOnInit(){
      this.store.dispatch(loadCountries())
   }
+  handleSearch(event: Event){
+    const input = event.target as HTMLInputElement
+    this.searchTerm.next(input.value)
+}
 
-  handleCardClick(name: string){
-   
-    
-  }
 
 }
 
